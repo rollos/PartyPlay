2acee32e56e4f98bb8d4f62bb7f941dda314489e
rollos <woodymelling@gmail.com>
2017-09-13 14:20:09 -0600

    initial commit with models

---
+++ PartyPlay/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200, primary_key=True)
+    url = models.CharField(max_length=200, default=str(name))
+    public = models.BooleanField(default=True)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+
+
+    def get_absolute_url(self):
+
+        return reverse('room-detail-view', args=[str(self.id)])
+
+
+class Video(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    video_url = models.CharField(max_length=2083)
+    video_name = models.CharField(max_length=200)
+    uploader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    rank = models.IntegerField(default=0)
+    played = models.BooleanField(default = False)
